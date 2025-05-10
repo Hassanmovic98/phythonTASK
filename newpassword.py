ca23c0da9fadd2dee7a5ff77d75428adb2667716
Hassanmovic98 <salakohassanmobolaji@gmail.com>
2025-05-10 12:07:00 +0100

    password task

---
+++ newpassword.py
@@ -0,0 +1,14 @@
+password = int(input('please enter your password:'))
+length = len(password)
+
+if length > 0 and length < 8:
+	print('too weak')
+
+elif length == 8:
+	print(' still weak but go ahead')
+
+elif length > 8 and length <= 18:
+	print('now its strong')
+
+elif length > 16:
+	print ('strong enough')
