a84c34704f22158bee0a706dd1d1a786e0123f19
Hassanmovic98 <salakohassanmobolaji@gmail.com>
2025-05-27 15:02:50 +0100

    phase gate

---
+++ pythonphasegate/To- Do- List/To_do_list_manager.py
@@ -0,0 +1,58 @@
+print("To Do List Manager")
+
+checklist = []
+
+def display_menu():
+	print("To do List Manager")
+	print("1.Add a task")
+	print("2.View tasks")
+	print("3.Mark tasks as as Complete")
+	print("4. Delete a task")
+	print("5. Exit")
+	
+
+def add_a_task(menu):
+	checkout.append (description- description)
+	print("Welcome,task has been included")
+
+def view_tasks(menu):
+	if not checklist:
+	       (checklist- Description)
+	       print("expenses has been charged")
+
+def delete_tasks(menu):
+	if menu == 4:
+		 print("task deleted") 
+
+
+def main():
+	 while True:
+		 display_menu ()
+		print("To do Do List Manager")
+			print("\n Choice:\n1. Add a task\n2. View tasks\n3. Mark tasks as complete\n4. Delete a task\n5. Exit")
+			Choice = int (input ("Task manager choose option between 1 - 5"))
+
+			if choice == "1":
+					description = ("Add a task")
+					result =(description)
+					print(result)
+		
+			elif choice == "2":
+				 	 description =("view tasks")
+				  	result =(description)
+				  	print(result)
+
+			elif choice == "3":
+					 description = ("Mark as complete")
+				 	result=(description)
+				 	print(result)
+
+			elif choice == "4":
+					description = ("Delete a task")
+					result=(description)
+					print(result)
+
+			elif choice == "5":
+				       description = ("Exit")
+			       		result=(description)
+			       		print(result)
---
+++ pythonphasegate/To- Do- List/Todomanagerlisttest.py
@@ -0,0 +1,6 @@
+import To_do_manager_List
+import Unittest
+
+class Testcube(unittest.Testcase):
+	def test_that_to_do_list_manager_exists(self)
+		to_do_list_manager.get_to_do_list_manager
