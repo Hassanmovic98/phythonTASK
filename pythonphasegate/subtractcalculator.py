1dd31846971338372829cbc38594e63b8523481f
Hassanmovic98 <salakohassanmobolaji@gmail.com>
2025-06-02 17:34:19 +0100

    subtractcalculator

---
+++ pythonphasegate/subtractcalculator.py
@@ -0,0 +1,13 @@
+import random 
+correctCount = 0
+count = 0
+
+while (count < 10)
+	number1 = random.randint(1,100)
+	number2 = random.randint(1,100)
+	if(number1 < number2):
+			temp = number1
+			number2 = number2
+
+		
+	
